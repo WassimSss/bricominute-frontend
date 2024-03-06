22f9c79c2cd2a145ae0db4a48e2dcd6d3d16aa32
WassimSss <tojiikan@gmail.com>
2024-03-06 09:47:14 +0100

    service complet avec backend

--- App.js
+++ App.js
@@ -8,39 +8,52 @@ import MessageScreen from './screens/consumer/MessageScreen';
 import ServiceScreen from './screens/consumer/ServiceScreen';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+
+import consumerServices from './reducers/consumerServices'
+
+const store = configureStore({
+  reducer: { consumerServices },
+});
+
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName = '';
+    <Provider store={store}>
 
-          if (route.name === 'Home') {
-            iconName = 'home';
-          } else if (route.name === 'Service') {
-            iconName = 'plus-square-o';
-          } else if (route.name === 'Message') {
-            iconName = 'wechat';
-          } else if (route.name === 'Profil') {
-            iconName = 'user';
-          }
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName = '';
 
-          return <FontAwesome name={iconName} size={size} color={color} />;
-        },
-        tabBarStyle: { backgroundColor: '#786396' }, // Changez 'blue' par la couleur de fond souhaitée
-        tabBarActiveTintColor: '#ffffff',
-        tabBarInactiveTintColor: '#979797',
-        headerShown: false,
-      })}>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Service" component={ServiceScreen} />
-        <Tab.Screen name="Message" component={MessageScreen} />
-        <Tab.Screen name="Profil" component={ProfilScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+            if (route.name === 'Home') {
+              iconName = 'home';
+            } else if (route.name === 'Service') {
+              iconName = 'plus-square-o';
+            } else if (route.name === 'Message') {
+              iconName = 'wechat';
+            } else if (route.name === 'Profil') {
+              iconName = 'user';
+            }
+
+            return <FontAwesome name={iconName} size={size} color={color} />;
+          },
+          tabBarStyle: { backgroundColor: '#786396' }, // Changez 'blue' par la couleur de fond souhaitée
+          tabBarActiveTintColor: '#ffffff',
+          tabBarInactiveTintColor: '#979797',
+          headerShown: false,
+        })}>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Service" component={ServiceScreen} />
+          <Tab.Screen name="Message" component={MessageScreen} />
+          <Tab.Screen name="Profil" component={ProfilScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
+
 }
 
 const styles = StyleSheet.create({
